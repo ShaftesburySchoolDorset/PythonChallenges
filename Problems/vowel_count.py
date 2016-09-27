b0c0395c65183311b99dda5a5f27e9c46f261a75
George Complin <gcomplin@gmail.com>
2016-09-27 13:14:28 +0100

    added vowel_count challenge

---
+++ Problems/vowel_count.py
@@ -0,0 +1,27 @@
+#Vowel Count
+
+This is a simple problem to get you introduced to string processing. We will be
+given a string of text and for each of them we want to know the number of vowels
+(i.e. letters a, e, i, o, u).
+
+Though simple, this technic is important in cipher-breaking approaches.
+
+##Example:
+
+Inputs:
+```
+4
+abracadabra
+pear tree
+o a kak ushakov lil vo kashu kakao
+my pix
+```
+
+Outputs:
+```
+0
+5
+4
+13
+1
+```
