ba61ed785c7afd2663187d504c9e74fc63c2094f
George Complin <gcomplin@gmail.com>
2016-10-04 18:50:33 +0100

    New recursion problems added

---
+++ Problems/7_recursion_vs_iteration_1.py
@@ -0,0 +1,11 @@
+
+
+
+"""
+ Given a list: a=[2,9,6,8,3], develop at least two iterative
+ solutions to print out every item on this list, from the first
+ to the last. One of the solutions may not use the 'range' function.
+ """
+
+for i in a:
+  print(i)
---
+++ Problems/8_banner_program.py
@@ -0,0 +1,25 @@
+"""
+Banner programs can create really nice effects, like this:
+
+******
+Xephos
+******
+
+The problem is that sometimes this happens:
+
+--------------
+Christopher Thompson III
+--------------
+
+Write a program that can take an input like this:
+
+'TJ | Christopher Thompson III'
+
+and output this:
+
+******   ****************************
+* TJ *   * Christopher Thompson III *
+******   ****************************
+
+
+"""
