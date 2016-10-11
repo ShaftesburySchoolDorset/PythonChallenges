2a499f80c537c3e3609504bb80415dc4cf14c1e7
George Complin <gcomplin@gmail.com>
2016-10-11 12:26:01 +0100

    Added challenge 12

---
+++ Problems/12_fib_hard.py
@@ -0,0 +1,39 @@
+# Introduction
+
+"""
+According to wikipedia; in mathematics, the Fibonacci numbers are the numbers in
+the following integer sequence, called the Fibonacci sequence, and characterized
+by the fact that every number in it is the sum of the two preceding ones:
+
+1, 1, 2, 3, 4, 8, 13, 21, 34, 55, 89, 144
+
+A number in a sequence (Fn), can be worked out wiht F(n-1) + F(n-2), where
+F0 = 1, and F1 = 1. Therefore, F2 = 2
+
+"""
+
+# Part 1 - Iterative Solution
+
+def fi(n):
+    return #?
+
+# Part 2 - Recursive Solution
+
+def fr(n):
+    return #?
+
+# Part 3 - Mathmatical Solution
+
+#todo
+
+def mf(n):
+    return #todo?
+
+# Part 4 - Range
+
+def SubFib(startNumber, endNumber):
+    return #?
+
+for i in SubFib(0, 10):
+    print i
+
